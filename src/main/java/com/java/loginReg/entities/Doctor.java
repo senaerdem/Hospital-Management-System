272a8cef07fed442ade6887ffcec41bb9b97913f
senaerdem <senanurerdem07@gmail.com>
2024-11-07 14:38:43 +0300

    Creating Doctor Entity

---
+++ src/main/java/com/java/loginReg/entities/Doctor.java
@@ -0,0 +1,85 @@
+package com.java.loginReg.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+	
+	@Column(name = "specialization")
+	private String specialization;
+	
+	@Column(name = "working_days")
+	private String workingDays;
+	
+	@Column(name = "working_hours")
+	private String workingHours;
+	
+	public Doctor(User user, String specialization, String workingDays, String workingHours) {
+		super();
+		this.user = user;
+		this.specialization = specialization;
+		this.workingDays = workingDays;
+		this.workingHours = workingHours;
+	}
+
+	public Doctor() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public String getWorkingDays() {
+		return workingDays;
+	}
+
+	public void setWorkingDays(String workingDays) {
+		this.workingDays = workingDays;
+	}
+
+	public String getWorkingHours() {
+		return workingHours;
+	}
+
+	public void setWorkingHours(String workingHours) {
+		this.workingHours = workingHours;
+	}
+
+}
