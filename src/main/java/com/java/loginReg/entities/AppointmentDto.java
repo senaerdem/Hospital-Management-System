4cc3a4c7e60682e0ecf6f8fc05a06b62b9147fae
senaerdem <senanurerdem07@gmail.com>
2024-11-14 18:39:42 +0300

    Appointment Dto oluşturuldu

---
+++ src/main/java/com/java/loginReg/entities/AppointmentDto.java
@@ -0,0 +1,43 @@
+package com.java.loginReg.entities;
+
+public class AppointmentDto {
+
+	private Long doctorId;
+    private Long patientId;
+    private String day;
+    private String time;
+
+    // Getters and Setters
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+}
